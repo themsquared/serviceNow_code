61b029adb8c5ca852468c331555ee9a905ea7b48
Mike Moore <themsquared@users.noreply.github.com>
2019-03-04 06:49:01 -0500

    More fancy and dynamic version of this.

--- instance_cmdb_query.py
+++ instance_cmdb_query.py
@@ -1,4 +1,5 @@
 from servicenow import ServiceNow as sn
+import requests
 
 
 # EXAMPLE USE OF SERVICENOW TAGS SYNC
@@ -15,10 +16,26 @@ user = '---'
 pwd = '---'
 
 # Datadog API & APP Keys
-api_key = '---'
-app_key = '---'
+API_KEY = '---'
+APP_KEY = '---'
+DATADOG_PREFIX = 'app'
 
-# Hostname that is shared between a servicenow registered server & Datadog
-hostname = "lnux100"
+# Go get the JSON permalink to the :
+url = "https://"+DATADOG_PREFIX+".datadoghq.com/reports/v2/overview?
+api_key="+API_KEY+"&application_key="+APP_KEY+"&metrics=avg%3Aaw
+s.ec2.cpuutilization%2Cavg%3Aazure.vm.percentage_cpu%2Cavg%3Agcp
+.gce.instance.cpu.utilization%2Cavg%3Asystem.cpu.idle%2Cavg%3Asy
+stem.cpu.iowait%2Cavg%3Asystem.load.norm.
+15%2Cavg%3Avsphere.cpu.usage&with_apps=true&with_sources=true&wi
+th_aliases=true&with_meta=true&with_mute_status=true&with_tags=t
+rue"
 
-sn.updateTags(hostname,sn.attributes(sn.findSysId(url, user, pwd, headers)), api_key, app_key)
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+host_json = data['rows']
+
+# For each host in Datadog, look up the corresponding item in SNow and tag it.
+for host in host_json:
+  hostname = host["name"]
+  aliases = host["aliases"]
+  sn.updateTags(hostname,sn.attributes(sn.findSysId(url, user, pwd, headers)), API_KEY, APP_KEY)
